aed8b550db90f3507c94dbb2bdeba0f34eb6f0f2
EMSD92 <diasevandro92@gmail.com>
2024-01-23 18:18:44 -0300

    1

---
+++ moduloD/aula11/ex008.js
@@ -0,0 +1,11 @@
+//console.log('la consola funcionó correctamente')
+
+var vel = 50.5
+
+console.log(`la velocidad de tu carro es de ${vel}`)
+
+if (vel>60){
+    console.log('passaste la velocidad maxima permitida, MULTADO')
+}
+
+console.log('maneje siempre usando el cinturón de seguridad')
---
+++ moduloD/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Nascendo em ${pais}, Você é`)
+if (pais =='Brasil'){
+    console.log('Brasileiro')
+}else{
+    console.log('Estrangeiro')
+}
