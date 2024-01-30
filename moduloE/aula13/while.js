4206195f21a861811a47ae9717a96bb474142fba
EMSD92 <diasevandro92@gmail.com>
2024-01-29 23:41:33 -0300

    1

---
+++ moduloE/aula13/while.js
@@ -0,0 +1,15 @@
+
+//while do
+var cont = 10
+while (cont > 0){
+    console.log(cont)
+    cont--
+}
+console.log('Fim loop while do')
+
+//do while
+do {
+    console.log(cont)
+    cont ++
+}while (cont < 10)
+console.log ('fim do loop do while')
---
+++ moduloE/aula14/for.js
@@ -0,0 +1,6 @@
+//o for utiliza uma sintaxe que ja define a variavel, faz a verificação e em seguida o incremento, td na mesma linha
+
+for (var c = 1/*definiu a variavel c com o valor 1*/; c <= 5/*verificou se o valor é menor ou igual a 5*/; c++/*faz o incremento*/){
+    console.log(c)
+}
+console.log ('fim do prgm')
